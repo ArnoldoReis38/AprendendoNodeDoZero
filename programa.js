23eb3db446c41796dcbc360b6d74361c0fef24f0
Arnoldo Reis <arnoldoprofissional@gmail.com>
2022-05-06 23:30:13 -0300

    Tarefa 01

---
+++ programa.js
@@ -0,0 +1,18 @@
+//Tarefa 01
+//Criar um novo projeto de NodeJS
+//Crie um arquivo para aplicação com o nome programa
+//No arquivo crie duas variáveis e imprima a soma delas
+//Execute o arquivo e verifique a resposta no terminal
+
+const num1 = 10
+const num2 = 5
+console.log(num1 + num2)
+
+const soma = num1 + num2
+console.log(soma)
+
+function Tarefa01(numero1, numero2) {
+    
+    console.log(numero1 + numero2)
+}
+Tarefa01(num1, num2)
